e41d2787472d1381fccec528a54af7862d1cd1cc
L1verly <artemartem7975@gmail.com>
2026-02-13 14:57:24 +0100

    basic colors and text styles setup

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -3,8 +3,10 @@ import SearchOrder from "../features/order/SearchOrder";
 
 export default function Header() {
   return (
-    <header>
-      <Link to="/">Fast React Pizza Ltd.</Link>
+    <header className="bg-yellow-500 uppercase">
+      <Link to="/cart" className="tracking-widest">
+        Fast React Pizza Ltd.
+      </Link>
       <SearchOrder />
     </header>
   );
