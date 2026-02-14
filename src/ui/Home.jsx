993e92d41bfbfc9238344e96e37c1584d9e9b9ff
L1verly <artemartem7975@gmail.com>
2026-02-14 10:49:11 +0100

    Responsive design

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -3,10 +3,10 @@ import CreateUser from "../features/user/CreateUser";
 function Home() {
   return (
     <div className="my-10 text-center sm:my-16">
-      <h1 className="mb-8 text-center text-xl font-semibold text-stone-700">
+      <h1 className="mb-8 text-center text-xl font-semibold text-stone-700 sm:text-2xl md:text-3xl">
         The best pizza.
         <br />
-        <span className="text-yellow-500">
+        <span className="text-yellow-400">
           Straight out of the oven, straight to you.
         </span>
       </h1>
