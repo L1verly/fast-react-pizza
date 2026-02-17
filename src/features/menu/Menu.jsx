8e4ba206338b1dc9a61d9ddfcb74e181fc90ecb6
L1verly <artemartem7975@gmail.com>
2026-02-17 21:48:51 +0100

    cleanup

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -2,7 +2,6 @@ import { useLoaderData } from "react-router-dom";
 import MenuItem from "./MenuItem";
 function Menu() {
   const menu = useLoaderData();
-  console.log(menu);
 
   return (
     <ul className="divide-y divide-stone-200 px-2">
